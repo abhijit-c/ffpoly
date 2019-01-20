04ff535e69685e6d96b45a997cf64386b7e5408e
abhijit-c <abhijit9331@gmail.com>
2019-01-20 16:14:40 -0500

    How do I implement the partial factorial in a computationally efficient manner? Also need to begin figuring out a gcd.

---
+++ ffpoly/diff_ffpoly.cc
@@ -0,0 +1,17 @@
+/* File: diff_ffpoly.cc
+ * Author: Abhijit Chowdhary */
+#include "ffpoly.h"
+
+/* Computationally efficient way to compute the partial factorial?
+ffpoly
+ffpoly::diff(unsigned long long n)
+{ 
+	ffpoly diffpoly(characteristic);
+	diffpoly.deg = std::max(0, deg - n);
+	diffpoly.coef[0] = 
+	for (unsigned long long i = n+1; i < deg; i++)
+	{
+		diffpo
+	}
+}
+*/
